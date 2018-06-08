76851d202973c06bebff947479056d381ab54779
Chandler May <chandler.j.may@gmail.com>
2018-06-08 17:31:19 -0400

    echo

--- env.cmd
+++ env.cmd
@@ -1,5 +1,3 @@
-@echo off
-
 IF "%PYTHON:~12,3%"=="x64" (
     call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
     call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
